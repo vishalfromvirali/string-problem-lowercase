3397b4c67d99c9c782f6dd7ac0c10f47b367d696
vishalfromvirali <vishalfromvirali@gmail.com>
2024-11-07 01:03:45 +0530

    Add files via upload

---
+++ lowercase.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class replace{
+    public static void main(String[]args){
+        String str1 = new  String();
+        str1 =str1+"HELLO VISHAL";
+        
+        String str2 = new String("vishal");
+        String result1 = str1.toLowerCase();
+
+        System.out.println(result1);
+
+        
+}
+}
